a5f9d1ce82d5cc7d90aee8662e305638ff58977a
SangHyun Jung <wjdtkdgus876@naver.com>
2025-10-03 16:10:01 +0900

    feat: toggleDone, deleteTodo 함수 구현 (수정 버튼 레이아웃 미완)

--- week3/JungSangHyun/index.js
+++ week3/JungSangHyun/index.js
@@ -1,11 +1,12 @@
 let todos = [];
 
+// DOM에서 태그를 가리키는 객체 -> DOM조작 메소드 사용가능 
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); 
   const text = input.value.trim();
   if (text) {
     const newTodo = {
@@ -18,12 +19,25 @@ form.addEventListener("submit", (e) => {
   }
 });
 
-function deleteTodo(id) {}
+function deleteTodo(id) {
+  todos = todos.filter(todo => todo.id !== id)
 
-function toggleDone(id) {}
+  render();
+}
+
+function toggleDone(todo) {
+  if (todo.done) {
+    todo.done = false;
+  }
+  else {
+    todo.done = true;
+  }
+
+  render();
+}
 
 function render() {
-  list.innerHTML = "";
+  list.innerHTML = ""; // todo-list 초기화 
   todos.forEach((todo) => {
     const li = document.createElement("li");
     li.className = todo.done ? "done" : "";
@@ -31,17 +45,24 @@ function render() {
     const span = document.createElement("span");
     span.textContent = todo.text;
     span.style.cursor = "pointer";
-    span.onclick = () => toggleDone(todo.id);
+    span.onclick = () => toggleDone(todo);
 
     const delBtn = document.createElement("button");
     delBtn.textContent = "삭제";
     delBtn.onclick = () => deleteTodo(todo.id);
 
+    const updateBtn = document.createElement("button");
+    updateBtn.textContent = "수정";
+    updateBtn.onclick = () => updateTodo(todo.id);
+
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(updateBtn);
     list.appendChild(li);
   });
 }
+
+// todo localStorage 사용까지 
 
 // JS로 삭제버튼 만든 것 처럼 만들기 
 function updateTodo() {
