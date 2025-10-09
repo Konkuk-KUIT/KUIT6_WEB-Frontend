a0840ad838148eecfa76a5eac96a2cfcbb83269a
Seoyun <sseoyunjjang343@gmail.com>
2025-10-09 14:41:10 +0900

    step 1 commit

---
+++ week4/SeoyunJang/src/model/navModel.js
@@ -0,0 +1,9 @@
+export const navModel = [
+    { name: "홈", iconSrc : "icons/home.svg" },
+    { name: "동네생활", iconSrc : "icons/news.svg" },
+    { name: "내 근처", iconSrc : "icons/place_marker.svg" },
+    { name: "채팅", iconSrc : "icons/chat.svg" },
+    { name: "나의 당근", iconSrc : "icons/user.svg" }
+];
+
+export default navModel;
