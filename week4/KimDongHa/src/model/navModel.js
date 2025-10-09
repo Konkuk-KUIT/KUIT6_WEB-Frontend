eba13080f4d2b518f5476976b9dd0916e4ed2aee
eastha10 <kdh9158kdh@gmail.com>
2025-10-09 15:11:50 +0900

    [4주차] 당근마켓 화면 React로 구현하기

---
+++ week4/KimDongHa/src/model/navModel.js
@@ -0,0 +1,9 @@
+const navModel = [
+  { name: "홈", iconSrc: "bottomnav/Home.svg" },
+  { name: "동네생활", iconSrc: "bottomnav/News.svg" },
+  { name: "내 근처", iconSrc: "bottomnav/Place Marker.png" },
+  { name: "채팅", iconSrc: "bottomnav/Chat.png" },
+  { name: "나의 당근", iconSrc: "bottomnav/User.png" },
+];
+
+export default navModel;
