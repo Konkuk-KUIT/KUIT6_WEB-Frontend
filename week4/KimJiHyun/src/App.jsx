48d2d105b78dfc82d6605834f62630c669b879ba
jyun27 <kimjyun27@gmail.com>
2025-10-11 00:42:06 +0900

    feat: step1 완료

---
+++ week4/KimJiHyun/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './components/Home.jsx';
+
+const Header = () => {};
+
+const Content = () => {};
+
+const BottomNav = () => {};
+
+function App() {
+    return <Home />;
+}
+
+export default App;
