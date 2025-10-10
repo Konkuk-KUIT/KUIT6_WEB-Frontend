2c79d28961420ee53eed38989ece6ea8519d00b6
eungehby <evthis4yeoniverse@gmail.com>
2025-10-11 02:40:17 +0900

    commit message: step1 complete

---
+++ week4/ShinBoYeon/src/model/navModel.js
@@ -0,0 +1,7 @@
+export const navModel = [
+    { name: "홈", iconSrc: "/home.svg" },
+    { name: "동네생활", iconSrc: "/news.svg" },
+    { name: "내 근처", iconSrc: "/place-marker.svg" },
+    { name: "채팅", iconSrc: "/chat.svg" },
+    { name: "나의 당근", iconSrc: "/user.svg" }
+];
