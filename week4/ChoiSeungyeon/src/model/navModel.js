b7b74c4cf725300cf1d9753992cf68aa0594959e
haruby2357 <haruby2357@gmail.com>
2025-10-10 21:56:34 +0900

    feat: step1 complete

---
+++ week4/ChoiSeungyeon/src/model/navModel.js
@@ -0,0 +1,7 @@
+export const navModel = [
+  { name: "홈", iconSrc: "assets/Home.svg" },
+  { name: "동네생활", iconSrc: "assets/News.svg" },
+  { name: "내 근처", iconSrc: "assets/Place_Marker.svg" },
+  { name: "채팅", iconSrc: "assets/Chat.svg" },
+  { name: "나의 당근", iconSrc: "assets/User.svg" },
+];
