a3ea32f9461622b935c82a03be7652e00090d016
SangHyun Jung <wjdtkdgus876@naver.com>
2025-10-10 23:09:49 +0900

    feat: step1 구현

---
+++ week4/JungSangHyun/src/model/navModel.js
@@ -0,0 +1,7 @@
+export const navModel = [
+    { name: "홈", iconSrc: "icon/home.svg" },
+    { name: "동네생활", iconSrc: "icon/news.svg" },
+    { name: "내 근처", iconSrc: "icon/place-marker.svg" },
+    { name: "채팅", iconSrc: "icon/chat.svg" },
+    { name: "나의 당근", iconSrc: "icon/user.svg" }
+];
