f97d29cb61947af31d01301900d927f4ea7e10e2
JunSeoChoi <evan7484@gmail.com>
2025-10-30 15:38:38 +0900

    Feat: 5주차 실습 코드 정적 구현

---
+++ week5/junseochoi/junseochoi/src/product/ProductRow.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function ProductRow({ name, price }) {
+  // 실습:
+  //   필수: onEditProduct를 통해 상품 정보 수정
+  //   선택: onDeleteProduct를 통해 상품 삭제
+  //   리팩토링까지 할 수 있으면 좋을 듯
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{price}</td>
+      <td>
+        <button>✏️</button>
+      </td>
+      <td>
+        <button>❌</button>
+      </td>
+    </tr>
+  );
+}
