3a92cfd3ad17a11d3ffeb7982af9d5a804c0c9a7
yuna <yjeong1217@gmail.com>
2025-11-02 10:09:03 +0900

    feat: 수정, 삭제 버튼 추가

--- week5/JeongYuna/src/product/ProductRow.jsx
+++ week5/JeongYuna/src/product/ProductRow.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+
 export function ProductRow({ product }) {
     return (
         <tr>
-        {product.stocked? <td>{product.name}</td> : <td style={{color: 'red'}}>{product.name}</td>}
+        {product.stocked?
+            <td>{product.name}</td>
+            : <td style={{color: 'red'}}>{product.name}</td>}
         <td>{product.price}</td>
+        <td className="edit-product">
+            <button><img src="/src/assets/pencil.png"></img></button>
+            <button><img src="/src/assets/trashcan.png"></img></button>
+        </td>
         </tr>
     )
 }
