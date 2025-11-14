75438229e7964eb583c53ad0d11f3865496193b2
SangHyun Jung <wjdtkdgus876@naver.com>
2025-11-14 15:30:17 +0900

    feat: 초기세팅 + 라우팅 구현

---
+++ week7/JungSangHyun/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Router from './pages/Router'
+
+function App() {
+
+  return (
+    <>
+      <Router></Router>
+    </>
+  )
+}
+
+export default App
