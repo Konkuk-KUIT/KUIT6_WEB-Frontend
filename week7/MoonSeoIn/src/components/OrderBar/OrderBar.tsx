2a670fae135359a2c09510a4de08b4ee0133697f
SeOinm <anatjdls0716@gmail.com>
2025-11-20 13:37:33 +0900

    fix: orderbar 총액에서 배달비 제외

--- week7/MoonSeoIn/src/components/OrderBar/OrderBar.tsx
+++ week7/MoonSeoIn/src/components/OrderBar/OrderBar.tsx
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom";
 import useCartStore from "../../pages/Store/useCartStore";
 
 const OrderBar = () => {
-  const { store, getTotalPrice } = useCartStore();
+  const { getTotalPrice } = useCartStore();
   const totalPrice = getTotalPrice();
-  const finalPrice = store ? totalPrice + store.deliveryFee : totalPrice;
 
   return (
     <div className="fixed bottom-0 left-0 w-full bg-white rounded-t-2xl shadow-[0_-2px_8px_rgba(0,0,0,0.1)] p-4 flex justify-between items-center">
       <div className="flex flex-col">
         <span className="text-[#6B7684] text-[15px]">총 주문금액</span>
-        <span className="text-[17px]">{finalPrice.toLocaleString()}원</span>
+        <span className="text-[17px]">{totalPrice.toLocaleString()}원</span>
       </div>
 
       <Link to="/cart">
