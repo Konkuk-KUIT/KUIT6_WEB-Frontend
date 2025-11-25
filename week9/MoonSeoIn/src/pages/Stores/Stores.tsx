a18dfc012b428ae38a8fbb652231f663c3959753
SeOinm <anatjdls0716@gmail.com>
2025-11-25 16:36:26 +0900

    chore : 기본 세팅

---
+++ week9/MoonSeoIn/src/pages/Stores/Stores.tsx
@@ -0,0 +1,21 @@
+import stores from "../../models/stores";
+import StoreListItem from "../../components/StoreListItem/StoreListItem";
+import OrderBar from "../../components/OrderBar/OrderBar";
+
+export default function Stores() {
+  return (
+    <>
+      <div className="px-6 pt-6">
+        <button onClick={() => window.history.back()} className="cursor-pointer">
+          <img src="/src/assets/chevron-left.svg" className="w-6 h-6 mb-4" />
+        </button>
+        <h2 className="text-[26px] font-bold mb-3">샐러드</h2>
+
+        {stores.map((store, index) => (
+          <StoreListItem key={store.id} ranking={index + 1} store={store} />
+        ))}
+      </div>
+      <OrderBar />
+    </>
+  );
+}
